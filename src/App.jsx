64121eb78b68bf7e5e0ac9eb9c5ba1eb0a92ea78
Gianni Schiralli <gschiralli@myseneca.ca>
2023-03-22 13:27:22 -0400

    added linkedList methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import { LinkedList } from "./models/LinkedList";
 
 function App() {
   const [theme, setTheme] = useState("light");
-
+  const initList = new LinkedList();
   return (
     <div className={`bg-${theme}`} style={{ height: "100vh" }}>
       <Header theme={theme} setTheme={setTheme} />
--- src/models/LinkedList.jsx
+++ src/models/LinkedList.jsx
@@ -1,13 +1,98 @@
 export class LinkedList {
   constructor(head = null) {
     this.head = head;
-    this.length = 0;
+    this.size = 0;
+  }
+
+  add(value, color) {
+    let node = new ListNode(value, color);
+    let current;
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  insertAt(value, color, index) {
+    if (index < 0 || index > this.size) {
+      throw new Error("Please enter a valid index");
+    } else {
+      let node = new ListNode(value, color);
+      let current, prev;
+      current = this.head;
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        let i = 0;
+        while (i < index) {
+          i++;
+          prev = current;
+          current = current.next;
+        }
+        node.next = current;
+        prev.next = node;
+      }
+      this.size++;
+    }
+  }
+  removeFrom(index) {
+    if (index < 0 || index >= this.size)
+      throw new Error("Please Enter a valid index");
+    else {
+      let curr,
+        prev,
+        it = 0;
+      curr = this.head;
+      prev = curr;
+
+      // deleting first element
+      if (index === 0) {
+        this.head = curr.next;
+      } else {
+        // iterate over the list to the
+        // position to removce an element
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr.next;
+        }
+
+        // remove the element
+        prev.next = curr.next;
+      }
+      this.size--;
+
+      // return the remove element
+      return curr.element;
+    }
+  }
+  reverse() {
+    if (this.size === 0) return;
+
+    let prev = null;
+    let current = this.head;
+    let next = null;
+    while (current != null) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    this.head = prev;
+    return this;
   }
 }
 class ListNode {
   constructor(value, color) {
     this.value = value;
-    this.next = null;
     this.color = color;
+    this.next = null;
   }
 }
